375ecbee9ce40f5b59376726c90f4edaceb40798
Caroline060 <nanatygumbio@gmail.com>
2025-03-19 16:28:51 -0300

    Atividade dia 17/03

---
+++ src/components/Rodape/Rodape.tsx
@@ -0,0 +1,13 @@
+import estilo from "./Rodape.module.css"
+
+function Rodape() {
+    return (
+        <footer className={estilo.rodape}>
+            <p>Desenvolvido por: Nathália Gumbio</p>
+            <p>Copyright</p>
+        </footer>
+    );
+
+}
+
+export default Rodape;
